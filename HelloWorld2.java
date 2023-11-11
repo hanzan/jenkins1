0b022537b5db6b9f6b6756ddd3771a40d3b16cc0
Hanzan George <ghanzan@gmail.com>
2023-11-11 22:18:45 +0530

    Add files via upload

---
+++ HelloWorld2.java
@@ -0,0 +1,12 @@
+public class HelloWorld2 {
+	
+	public static void main (String args[])
+	{
+		System.out.println("Hello World");
+		
+		for (int i=0; i< 10; i++)
+		{
+			System.out.println("Hello " + i);
+		}
+	}
+}
